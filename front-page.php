27231d590e52b7fed96895749f0631131c1f22ce
eknox47 <edaigneault47@gmail.com>
2023-03-08 08:35:10 -0500

    c11 modifier template-parts

--- front-page.php
+++ front-page.php
@@ -5,17 +5,14 @@
 <main class="site__main">
     <code>front-page.php</code>
     <section class="blocflex">
-        <?php 
-            if (have_posts()) :
+      <?php if (have_posts()) :
                 while(have_posts()) : the_post(); ?>
-                <?php if(in_category("galerie")){
-                    get_template_part("template-parts/categorie", "galerie");   
-                } else {
-                    get_template_part("template-parts/categorie", "note-wp");
-                }
+                <?php
+                $ma_categorie = "note-wp";
+                if(in_category("galerie")){$ma_categorie = "galerie";}
+                get_template_part("template-parts/categorie", $ma_categorie);   
                 endwhile;
-            endif;
-        ?>
+            endif; ?>
     </section>
 </main>
 <?php get_footer(); ?>
